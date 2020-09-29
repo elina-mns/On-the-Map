6c8242e31fd36d7d881eca74f58b7d1b8942e15a
elina-mns <ellie.mansurova@gmail.com>
2020-09-29 13:59:04 -0400

    Initial Commit

---
+++ On the Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  On the Map
+//
+//  Created by Elina Mansurova on 2020-09-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
